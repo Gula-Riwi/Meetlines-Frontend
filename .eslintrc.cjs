26fc912c6ce60dfcc44145db7479af6c4f009679
Jteban1 <sepesteban@gmail.com>
2025-12-16 00:18:52 -0500

    build: add ESLint configuration

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2022: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential'  // Less strict than vue3-recommended
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    'no-unused-vars': 'warn',
+    'no-undef': 'warn'  // Warn instead of error for undefined variables
+  }
+}
